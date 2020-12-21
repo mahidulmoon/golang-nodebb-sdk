17fe95c1c3a130703a36aae3532bb923e7c83fea
mahidulmoon <mahidulmoon@gmail.com>
2020-12-21 09:48:50 +0600

    setup successfull

---
+++ app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+
+	"go_nodebb_sdk/api"
+
+)
+
+func main(){
+	api.Runserver(":9004")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_nodebb_sdk
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
